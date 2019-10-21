bb902abc61c772dcba1ef48a564ca0ca0b8fe054
Mayra Vazquez-Sanchez <mayravsancez479@gmail.com>
2019-10-21 19:32:09 -0400

    Part 2 -- additional features

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Mayra Vázquez-Sánchez on 10/21/19.
+//  Copyright © 2019 MVS. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
